e88af82e5eaf90643ad09d97d508f4ee2da661d3
modmoto <simonheiss87@gmail.com>
2021-06-02 10:16:54 +0200

    fixo

--- ExampleGameBackend/Team.cs
+++ ExampleGameBackend/Team.cs
@@ -5,7 +5,7 @@ namespace ExampleGameBackend
     public class Team
     {
         public string TeamId { get; set; }
-        public List<string> PlayerIds { get; set; }
+        public List<PlayerIdentificationDto> PlayerIds { get; set; }
         public Mmr Mmr { get; set; }
     }
 }
